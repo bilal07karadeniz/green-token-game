f7ad07e83498698e9312aff59ca7faf01b3d3126
wekruh <wekruhh@gmail.com>
2024-11-30 15:06:19 +0300

    Update of the NTFs part.

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ const nfts = [
     { id: 3, name: "Hydropower NFT", income: 0.12 },
 ];
 
-// Creating NFT cards dynamically
-const nftList = document.querySelector('.nft-list');
-nfts.forEach(nft => {
-    const nftCard = document.createElement('div');
-    nftCard.classList.add('nft-card');
-    nftCard.innerHTML = `
-        <h3>${nft.name}</h3>
-        <p>Income: ${nft.income} ETH/day</p>
-    `;
-    nftList.appendChild(nftCard);
-});
-
 // Simulating NFT purchase
 function purchaseNFT(nftId) {
     const nft = nfts.find(n => n.id === nftId);
